e913be05cbbf22f759cf709d38b47555cb13ea01
Nguyễn Thanh Tuấn <tuantoi1000@gmail.com>
2024-01-24 11:58:29 +0700

    test

--- Project/ngu0120-project-java2/ecommerce_be/src/main/java/project/java2/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package project.java2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import project.java2.entity.Product;
-
-// JPA
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
